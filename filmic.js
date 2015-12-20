a1e952e08ce88210f7e768a92bd8dc75c952cdf3
Sahil Gupta <sahil.gupta@yale.edu>
2015-12-19 19:44:44 -0500

    simplify angular

--- filmic.js
+++ filmic.js
@@ -1,22 +1,14 @@
-var app = angular.module('filmicApp', [])
-  .controller('FilmicListController', function() {
-    var todoList = this;
-    todoList.todos = [
-      {text:'learn angular', done:true},
-      {text:'build an angular app', done:false}];
- 
-    todoList.addTodo = function() {
-      todoList.todos.push({text:todoList.todoText, done:false});
-      todoList.todoText = '';
-    };
- 
-  });
+var app = angular.module('filmicApp', []);
+
+app.controller('FilmicController', function($scope) {
+  $scope.test = "testtt"
+});
 
 
+// runs at the beginning
+app.run(function($scope, $window) {
 
-app.run(['$rootScope', '$window',  function($rootScope, $window) {
-
-  $rootScope.user = {};
+  $scope.user = {};
   
   // execute when the sdk is loaded
   $window.fbAsyncInit = function() {
@@ -30,38 +22,28 @@ app.run(['$rootScope', '$window',  function($rootScope, $window) {
     // watch login change
     FB.Event.subscribe('auth.authResponseChange', function(res) {
       if (res.status === 'connected') {
-
+        // get the user, and store in "$scope.user"
         FB.api('/me', function(res) {
-          $rootScope.$apply(function() { 
-            $rootScope.user = res; // important line
+          $scope.$apply(function() { 
+            $scope.user = res; // important line
           });
         });
-          
-        // the user object exists in "$rootScope.user"
-        console.log($rootScope.user);
-        console.log(res.authResponse);
-        
       } else {
         console.log("the user isn't logged in");
       }
     });
-
-
   };
 
   // immediately invoked function
   // load the facebook sdk
   (function(d){
     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-
     if (d.getElementById(id)) { return; }
-
     js = d.createElement('script');
     js.id = id;
     js.async = true;
     js.src = "https://connect.facebook.net/en_US/all.js";
-
     ref.parentNode.insertBefore(js, ref);
   }(document));
 
-}]);
+});
