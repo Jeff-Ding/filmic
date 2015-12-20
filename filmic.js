4faff70cb17834969f90c87611c6c75bed6e2a8f
Jeff-Ding <dingjeff3@gmail.com>
2015-12-19 19:51:01 -0500

    added getNeighbors() function

--- filmic.js
+++ filmic.js
@@ -47,3 +47,53 @@ app.run(function($scope, $window) {
   }(document));
 
 });
+
+//  me: object
+//    key: movies, songs, books
+//    value: array of liked page ID's for that medium
+//  friends: object
+//    key: friend ID
+//    value: "me" object for that friend
+//  enhanced: boolean
+//  threshold: percentile cutoff for closeness
+//    represented as int between 0 and 1
+//  return: array of [friend ID, closeness] pairs that exceed threshold
+var getNeighbors = function(me, friends, enhanced, threshold) {
+  var neighbors = [];
+  var length = 0;
+  
+  for (var id in friends.keys()) {
+    var friend = friends.id;
+    var closeness = sharedLikes(me, friend, movies);
+    
+    if (enhanced) {
+      closeness += sharedLikes(me, friend, songs);
+      closeness += sharedLikes(me, friend, books);
+    }
+    
+    neighbors.push([id, closeness]);
+    length++;
+  }
+  
+  neighbors.sort(function(a, b) {
+    return a[1] - b[1];
+  });
+  var cutoff = Math.ceil(length*threshold);
+  
+  return neighbors.slice(cutoff, length+1);
+};
+
+
+// returns common likes between user and friend
+// for specific medium (movies, songs, or books)
+var sharedLikes = function(me, friend, medium) {
+  var shared = 0;
+  for (var myTitle in me.medium) {
+    for (var friendTitle in friend.medium) {
+      if (myTitle === friendTitle) {
+        shared++;
+      }
+    }
+  }
+  return shared;
+};
